40bc6eacc2185ac46fa1fd4b444993bb25b36e16
muthu raveendran <muthuraveendran01@gmail.com>
2020-08-30 11:52:40 -0700

    Fixed Selenium version

--- src/main/java/newPratice/openGoogle.java
+++ src/main/java/newPratice/openGoogle.java
@@ -1,15 +1,15 @@
 package newPratice;
 
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class openGoogle {
 
     public static void main(String[] args) {
 
 //
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://www.youtube.com/");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/");
     }
 
 }
